16cd28b841ba75a164958f537ce9a15a060af016
Matt Lightfoot <m-lightfoot@hotmail.com>
2023-12-08 19:16:55 +0000

    Added current day to html through dayjs

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+// Dayjs plugins
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 dayjs.extend(window.dayjs_plugin_customParseFormat);
 
+// Query selectors
 const currentDayText = $("#currentDay");
 
 
 
+
+// Variable of the current day
 const currentDay = dayjs();
 
-console.log(currentDay.format('dddd, MMMM Do'))
+// Shows the current day
+currentDayText.text(currentDay.format('dddd, MMMM Do'));
+
+
+
